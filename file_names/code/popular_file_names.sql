ba1220c2ea4214c32a10dcef2c1ffded1148526a
idanacumen <idan@acumen.io>
2021-07-08 12:39:46 +0300

    and/or in file names

--- file_names/code/popular_file_names.sql
+++ file_names/code/popular_file_names.sql
@@ -65,3 +65,31 @@ cnt
 order by
 cnt
 ;
+
+# see results in and_or_in_code_file_names.csv
+select
+lower(REGEXP_EXTRACT(reverse(substr(reverse(file), 0, STRPOS(reverse(file), '/')))
+, concat(r'(?i)[_\.-](and|or)[_\.-]'))) as file_name_group
+, count(*) as cnt
+, avg(ccp) as ccp
+, avg(refactor_mle) as refactor_mle
+, avg(avg_coupling_code_size_cut) as avg_coupling_code_size_cut
+, avg(same_day_duration_avg) as same_day_duration_avg
+, avg(bug_prev_touch_ago) as bug_prev_touch_ago
+, avg(size) as size
+, avg(abs_content_ratio) as abs_content_ratio
+, avg(performance_rate) as performance_rate
+, avg(security_rate) as security_rate
+, avg(one_file_fix_rate) as one_file_fix_rate
+, avg(one_file_refactor_rate) as one_file_refactor_rate
+from
+general.file_properties
+where
+not is_test
+and
+code_extension
+group by
+file_name_group
+order by
+count(*) desc
+;
